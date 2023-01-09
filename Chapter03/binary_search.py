b0bf14743414e266e0e4e73e75d179859397b4c3
Karan <50290386+Sonawane-Karan26@users.noreply.github.com>
2023-01-09 18:26:29 +0530

    Update binary_search.py

--- Chapter03/binary_search.py
+++ Chapter03/binary_search.py
@@ -1,6 +1,6 @@
 def binary_search(arr, start, end, key):
     while start <= end:  
-        mid = start + (end - start)/2)
+        mid = start + (end - start)/2
         if arr[mid] == key:  
             return mid  
         elif arr[mid] < key:  
